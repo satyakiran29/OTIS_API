542cb70ee5ba2f0762a0a4d20b93c039e8e0f6bc
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2026-01-18 14:05:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}  Developed by Satya Kiran`)
 );
