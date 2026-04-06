90804f89c49554ede6bce0dd432f8c71983871e5
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2026-04-07 01:39:41 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,10 +3,10 @@ const { Resend } = require('resend');
 const sendEmail = async (options) => {
     try {
         console.log("Attempting to load RESEND_API_KEY:", typeof process.env.RESEND_API_KEY, `"${process.env.RESEND_API_KEY}"`);
-        const apiKey = process.env.RESEND_API_KEY || "re_jHbyi2JP_FNgroyAKiJnywd7WoCThVPuY";
+        const apiKey = process.env.RESEND_API_KEY || "re_86w9PjzF_ENxhJbLtzmD8fnGzUrE2K7HR";
         const resend = new Resend(apiKey);
         // Fallback to onboarding@resend.dev if a verified domain isn't available during testing
-        const fromEmail = process.env.RESEND_FROM_EMAIL || 'onboarding@psatyakiran.in';
+        const fromEmail = process.env.RESEND_FROM_EMAIL || 'otis@psatyakiran.in';
 
         const { data, error } = await resend.emails.send({
             from: `Otis Temple System <${fromEmail}>`,
